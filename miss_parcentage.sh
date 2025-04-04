0fb9de2386b5914f18b47dc9c02eccb14647c9de
nasuda <nasuda908@gmail.com>
2025-04-04 20:10:11 +0900

    add "miss_parcentage.sh"

---
+++ miss_parcentage.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+total_keytypes=$(($1+$2))
+echo $total_keytypes
--- sh/inse.sh
+++ sh/inse.sh
@@ -3,4 +3,4 @@
 yen=$1 num_of_correct_keys=$2 ave_num_of_keys=$3 num_of_typos=$4 course=$5
 echo "$yen $num_of_correct_keys $ave_num_of_keys $num_of_typos $course"
 
-echo "$1 $2 $3 $4 $5" >> yen.csv
+echo "$1 $2 $3 $4 $5" >> yen.txt
